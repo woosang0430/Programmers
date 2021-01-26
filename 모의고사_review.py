73fae99a420f6e69846b381bdaf7d1e4d3332c6e
Woosang <77317312+yws1502@users.noreply.github.com>
2021-01-26 19:25:13 +0900

    Update 모의고사_review.py

--- 모의고사_review.py
+++ 모의고사_review.py
@@ -10,17 +10,6 @@ chk = [0] * len(p)
 for num, answer in enumerate(answers):
     for i, v in enumerate(p):
         if answer == v[num % len(v)]:
-    #         def dict_tests(lst, len_answer):
-    #           if len_answer % len(lst) != 0:
-    #               test_lst = lst * (len_answer//len(lst)) + lst[:len_answer%len(lst)]
-    #           else:
-    #                test_lst = lst * (len_answer//len(lst))
-
-    #           return {k:v for k, v in enumerate(test_lst)}
             chk[i] += 1
 
 print([i+1 for i, v in enumerate(chk) if v == max(chk)])
-# dict_supoja = dict(zip(range(1,len(chk)+1), chk))
-# sorted(dict_supoja.values())
-# winner = [k for k, v in dict_supoja.items() if max(dict_supoja.values()) == v]
-# print(winner)
