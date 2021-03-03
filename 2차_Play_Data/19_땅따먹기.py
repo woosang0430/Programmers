877a0f3e27ef29ed8439ee19979107a560fe6b0e
Woosang <77317312+yws1502@users.noreply.github.com>
2021-03-03 22:05:41 +0900

    Update 19_땅따먹기.py

--- 2차_Play_Data/19_땅따먹기.py
+++ 2차_Play_Data/19_땅따먹기.py
@@ -1,11 +1,12 @@
 land = [[1, 2, 3, 5], [5, 6, 7, 100], [4, 3, 2, 1]]
-
+# 다른 사람 풀이 1
 for row in range(1, len(land)):
     for i in range(4):
         rest = set(range(4)) - {i}
         land[row][i] += max([land[row-1][idx] for idx in range(4) if idx in rest])
 print(max(land[-1]))
 
+# 다른 사람 풀이 2
 lst = []
 for i in range(1, len(land)):
     for j in range(4):
@@ -17,17 +18,17 @@ for i in range(1, len(land)):
         lst = []
 print(max(land[-1]))
 
+# 다른 사람 풀이 3 효율성 값
 for i in range(1, len(land)):
     for j in range(4):
         # 중복되는 인덱스를 뺀 리스트를 만들고 그중 제일 높은 수를 더해준다.
         land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
 print(max(land[-1]))
 
-# for i in range(1, len(land)):
-#     for j in range(4):
-#         print(land[i][j])
-#         land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
-#         print(land[i-1][:j], ",", land[i-1][j+1:])
-#         print(land[i][j])
-#         print()
-# print(max(land[-1]))
+# land[i-1][:j], land[i-1][j+1:]
+"""
+[] , [2, 3, 5]
+[1] , [3, 5]
+[1, 2] , [5]
+[1, 2, 3] , []
+"""
