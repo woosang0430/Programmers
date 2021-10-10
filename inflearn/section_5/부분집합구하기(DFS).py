c0f5c4dc6c96cd9fae603bc1e45a663f31d8d66d
Woosang <77317312+yws1502@users.noreply.github.com>
2021-10-10 18:25:57 +0900

    Add files via upload

---
+++ inflearn/section_5/부분집합구하기(DFS).py
@@ -0,0 +1,21 @@
+'''
+# input.txt
+3
+'''
+
+import sys
+sys.stdin = open('input.txt', 'rt')
+
+def DFS(i):
+  if i > n:
+    print(*[idx for idx in range(len(visited)) if visited[idx] == 1])
+    return
+  visited[i] = 1
+  DFS(i+1)
+  visited[i] = 0
+  DFS(i+1)
+
+if __name__ == '__main__':
+  n = int(input())
+  visited = [0] * (n+1)
+  DFS(1)
