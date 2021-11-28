54d952b63efe157173aaff079e6d6d7bbabf96dc
Woosang <yws1502@naver.com>
2021-11-28 20:56:19 +0900

    Update '부분수열의합.py' added code

--- python/1주차/부분수열의합.py
+++ python/1주차/부분수열의합.py
@@ -17,6 +17,28 @@ for i in range(1, N+1):
 
 print(answer)
 
+# 재귀를 이용한 완전 탐색
+# 파라미터로 현재의 값을 더한 경우와 더하지 않은 경우로 재귀를 돌린다.
+# idx가 리스트의 index를 넘기는 것을 종료조건으로 한다
+def solution(idx:int, sub_sum:int) -> None:
+    global answer
+
+    if idx == N: return
+
+    sub_sum += number_list[idx]
+    if sub_sum == goal:
+        answer += 1
+
+    solution(idx+1, sub_sum)
+    solution(idx+1, sub_sum - number_list[idx])
+
+if __name__  == "__main__":
+    N, goal = map(int, input().split())
+    number_list = list(map(int, input().split()))
+    answer = 0
+    solution(0, 0)
+
+    print(answer)
 """
 input
 5 0
